9673400ce3698f91293e21c8f7eafdd6338d721d
Harshit Anand <harshitanand94@gmail.com>
2015-04-24 13:54:55 +0530

    Installing django allauth

--- Social_Connect/settings.py
+++ Social_Connect/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
